3c2e9269aadd6fe095e01d647dd4d1ed59f7b6c9
xephyr <vinmay.nair90@gmail.com>
2014-04-20 18:32:30 +0530

    Factorial of numbers

--- factorial.rs
+++ factorial.rs
@@ -1,13 +1,26 @@
 use std::io::BufferedReader;
-use std::io::println;
-use std::io;
+    use std::io::println;
+    use std::io;
 
+    fn main() {
+    	let mut i = 1;
+    	let mut f = 1;    	
+    	let mut mynum = 0;
+        println("Enter a number:");
+        let mut reader = BufferedReader::new(io::stdin());
+        let input = reader.read_line().unwrap();
+        let num = from_str::<int>(input.slice_to(input.len() - 1));
 
-fn main() {
-    println("Enter something:");
-    let mut reader = BufferedReader::new(io::stdin());
-
-    let input = reader.read_line().unwrap();
-    println("Something that you typed:");
-    println(input);
-}
+        match num {
+                Some(number) => {
+                    mynum = number
+                }
+                None         => println("Hey, put in a number.")
+            }
+        while i <= mynum {
+        	f = f * i;
+        	i += 1;
+        }
+        println("Factorial is:");
+        println(f.to_str());
+    }
