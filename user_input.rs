6bbb210346bd14bf7e0856bc9558ab592ef1edea
xephyr <vinmay.nair90@gmail.com>
2014-04-20 13:11:39 +0530

    Getting input from User

---
+++ user_input.rs
@@ -0,0 +1,13 @@
+use std::io::BufferedReader;
+    use std::io::println;
+    use std::io;
+
+
+    fn main() {
+        println("Enter something:");
+        let mut reader = BufferedReader::new(io::stdin());
+
+        let input = reader.read_line().unwrap();
+        println("Something that you typed:");
+        println(input);
+    }
