4719304cf20b7bbe7492019315c66d9b8672c4c9
xephyr <vinmay.nair90@gmail.com>
2014-04-20 13:14:18 +0530

    Changes in file structure

--- user_input.rs
+++ user_input.rs
@@ -1,13 +1,13 @@
 use std::io::BufferedReader;
-    use std::io::println;
-    use std::io;
+use std::io::println;
+use std::io;
 
 
-    fn main() {
-        println("Enter something:");
-        let mut reader = BufferedReader::new(io::stdin());
+fn main() {
+    println("Enter something:");
+    let mut reader = BufferedReader::new(io::stdin());
 
-        let input = reader.read_line().unwrap();
-        println("Something that you typed:");
-        println(input);
-    }
+    let input = reader.read_line().unwrap();
+    println("Something that you typed:");
+    println(input);
+}
